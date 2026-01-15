35cc477e200cce6bdb2e91207d2fde24cdf5d5ca
ai-integrating <aiintegrating@gmail.com>
2026-01-15 09:40:04 -0500

    Update enqueue-job.js

--- api/enqueue-job.js
+++ api/enqueue-job.js
@@ -1,4 +1,4 @@
-js.function setCors(res) {
+function setCors(res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
